4a5e2a01c41fa67e550dd689755bef7131e3644c
Kyle Hovey <khoveyc@gmail.com>
2018-05-12 12:45:18 -0600

    Implemented encoding/decoding functionality for images.

--- src/js/index/app.js
+++ src/js/index/app.js
@@ -13,7 +13,7 @@ export default class App {
      * @type {Example}
      */
     this.image = new CanvasImage({
-      src : "../images/GEB.png",
+      src : "../images/GEB_enc.png",
       width : 1000,
       height : 1443,
       container : "#image"
