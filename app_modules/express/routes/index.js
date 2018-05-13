f2a55c07195739e67e6efe9fc800f08d3f06d142
Kyle Hovey <khoveyc@gmail.com>
2018-05-12 21:22:07 -0600

    Fixed relative links.

--- app_modules/express/routes/index.js
+++ app_modules/express/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title : "Node Boilerplate" });
+  res.render("index", { title : "Steganography Applet" });
 
   next();
 });
